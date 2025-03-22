eb1fd0997cd75f887fcd61ed68a283b265afac2b
rohitanookala <nookala.r@northeastern.edu>
2025-03-22 16:21:42 -0400

    navbar import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import logo from '../assets/logo.png';
+import Profile from '../components/Profile';
 
 export default function Home() {
   return (
