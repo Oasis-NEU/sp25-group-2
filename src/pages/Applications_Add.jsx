2d0e7f8888e27c7563b5150aeae5f9bfcad529f1
rohitanookala <nookala.r@northeastern.edu>
2025-03-22 15:10:53 -0400

    applications add page

---
+++ src/pages/Applications_Add.jsx
@@ -0,0 +1,9 @@
+import ApplicationAdd from '../components/ApplicationAdd';
+
+export default function Applications_Add() {
+    return (
+        <>
+        <ApplicationAdd />
+        </>
+    );
+}
