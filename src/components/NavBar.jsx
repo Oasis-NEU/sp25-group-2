15fafd3b0be93a2b8409a8e9c5bc06ee5dbc2d76
rohitanookala <nookala.r@northeastern.edu>
2025-03-22 14:54:38 -0400

    navbar, applications page + styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,24 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 export default function NavBar() {
+  const [showApplicationsDropdown, setShowApplicationsDropdown] = useState(false);
+  
+  const toggleApplicationsDropdown = () => {
+    setShowApplicationsDropdown(!showApplicationsDropdown);
+  };
+
   return (
     <nav className="horizontal-navbar">
       <ul className="navbar-titles">
-        <li className="navbar-page">
+        <li className="navbar-title">
           <a href="#YourAccount" className="navbar-page">Your Account</a>
         </li>
-        <li className="navbar-titles dropdown">
+        <li className="navbar-title dropdown">
           <a href="#SignOut" className="navbar-page">Sign Out</a>
           <a href="#Profile" className="navbar-page">Your Profile</a>
         </li>
-        <li className="navbar-titles dropdown">
+        <li className="navbar-title dropdown">
           <div 
             className="navbar-page dropdown-toggle" 
             onClick={toggleApplicationsDropdown}
@@ -18,20 +27,19 @@ export default function NavBar() {
           </div>
           {showApplicationsDropdown && (
             <div className="dropdown-menu">
-              <Link to="/applications" className="dropdown-titles">View Applications</Link>
-              <Link to="/add-application" className="dropdown-titles">Add Application</Link>
+              <Link to="/applications" className="dropdown-item">View Applications</Link>
+              <Link to="/add-application" className="dropdown-item">Add Application</Link>
             </div>
           )}
         </li>
-
-        <li className="navbar-titles">
+        <li className="navbar-title">
           <a href="#InterviewTips" className="navbar-page">Interview Tips</a>
         </li>
-        <li className="navbar-titles">
+        <li className="navbar-title">
           <a href="#Resources" className="navbar-page">Resources</a>
         </li>
       </ul>
     </nav>
-  )
+  );
 }
 
---
+++ src/pages/Applications.jsx
@@ -0,0 +1,11 @@
+import NavBar from '../components/NavBar';
+import ApplicationAdd from '../components/ApplicationAdd';
+
+export default function Applications() {
+    return (
+        <>
+        <NavBar />
+        <ApplicationAdd />
+        </>
+    );
+}
