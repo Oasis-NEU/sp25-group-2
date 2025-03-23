c3751668af3bf2c8e46f2641760e83259910498b
anjalisilina <silina.a@northeastern.edu>
2025-03-22 21:09:48 -0400

    fin

--- src/pages/InterviewTips.jsx
+++ src/pages/InterviewTips.jsx
@@ -1,13 +1,13 @@
 import NavBar from '../components/NavBar';
+import Layout from '../components/Layout';  
 import Tips from '../components/Tips';
 
 export default function InterviewTips() {
     return (
         <>
-            <NavBar />
-            <div className="interview-tips-container">
-                <Tips />
-            </div>
+        <NavBar />
+        <Layout />
+        <Tips />
         </>
     );
 }
